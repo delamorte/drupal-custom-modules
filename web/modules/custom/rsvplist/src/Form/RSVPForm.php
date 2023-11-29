5763eb2d3a178d3780060025a82248a1ac93e555
delamorte <dellamorte@disroot.org>
2023-11-29 17:10:14 +0200

    Add a RVSP custom module

---
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * A form to collect an email address for RSVP details.
+ */
+
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class RSVPForm extends FormBase
+{
+
+  public function getFormId(): string
+  {
+    return 'rsvplist_email_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    if (!(is_null($node))) {
+      $nid = $node->id();
+    } else {
+      $nid = 0;
+    }
+
+    $form['email'] = [
+      '#type' => 'textfield',
+      '#title' => t('Email address'),
+      '#size' => 25,
+      '#description' => t('We will send updates to the email address you provide'),
+      '#required' => TRUE,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('RSVP'),
+    ];
+    $form['nid'] = [
+      '#type' => 'hidden',
+      '#value' => $nid,
+    ];
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $submitted_email = $form_state->getValue('email');
+    $this->messenger()->addMessage(t('The form is working! You entered @entry',
+    ['@entry' => $submitted_email]));
+  }
+}
+
+
